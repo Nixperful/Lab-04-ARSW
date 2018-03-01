f586c5546be285386a06af4da9d049bf5bb0167b
n.osorio <nicolas.osorio@mail.escuelaing.edu.co>
2018-02-28 21:58:03 -0500

    Mejoras de despliege

--- src/main/java/edu/eci/arsw/myrestaurant/RestapidemoApplication.java
+++ src/main/java/edu/eci/arsw/myrestaurant/RestapidemoApplication.java
@@ -1,9 +1,12 @@
 package edu.eci.arsw.myrestaurant;
 
+import edu.eci.arsw.myrestaurant.restcontrollers.OrdersAPIController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackageClasses=OrdersAPIController.class)
 public class RestapidemoApplication {
 
 	public static void main(String[] args) {
