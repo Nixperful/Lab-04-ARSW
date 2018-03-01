b7535f37bd77f6d6748dfee4e77b454ace83c23b
n.osorio <nicolas.osorio@mail.escuelaing.edu.co>
2018-03-01 00:07:06 -0500

    Intento de arreglo

--- src/main/java/edu/eci/arsw/myrestaurant/RestapidemoApplication.java
+++ src/main/java/edu/eci/arsw/myrestaurant/RestapidemoApplication.java
@@ -1,11 +1,11 @@
 package edu.eci.arsw.myrestaurant;
 
-import edu.eci.arsw.myrestaurant.restcontrollers.OrdersAPIController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+
 public class RestapidemoApplication {
 
 	public static void main(String[] args) {
